09ff2824ffb4ce983049dc3bf98937f120825a25
Andrew Kelley <iodine73@gmail.com>
2017-11-14 14:56:43 -0500

    should be fully functional

--- controllers/articlesController.js
+++ controllers/articlesController.js
@@ -7,12 +7,14 @@ module.exports = {
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+  
   create: function(req, res) {
     db.Article
       .create(req.body)
       .then(dbModel => res.json(dbModel))
       .catch(err => res.status(422).json(err));
   },
+
   remove: function(req, res) {
     console.log(req.params.id);
     db.Article
